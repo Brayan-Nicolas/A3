83969736fc245341c7ce68c0ede454afe1370f11
Suno <misticbr@hotmail.com>
2025-09-21 11:00:13 -0300

    Initial commit

---
+++ src/main/ProgramaGestão.java
@@ -0,0 +1,15 @@
+package main;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
+
+public class ProgramaGestão {
+	public static void Main(String args[]) {
+		
+	}
+}
+
+
+
---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module A3 {
+	requires java.desktop;
+	requires java.sql;
+}
